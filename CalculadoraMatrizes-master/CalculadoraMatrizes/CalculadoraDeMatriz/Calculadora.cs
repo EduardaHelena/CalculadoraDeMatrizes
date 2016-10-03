776679d8af339ce80ac8f0851f87c02a3d8bc87a
Eduarda Helena <dudinhahelenaa@gmail.com>
2016-10-03 16:27:01 -0300

    NiceLayout
    better layout

---
+++ CalculadoraMatrizes-master/CalculadoraMatrizes/CalculadoraDeMatriz/Calculadora.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraDeMatriz
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoInversa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoTransposta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoDeterminante_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoSimetrica_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoOposta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        internal static void Showmodal()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void otaoMultiplicao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoInversaB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sair(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LeideForma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gerar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CalculadoraMatrizes-master/CalculadoraMatrizes/CalculadoraDeMatriz/FormMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CalculadoraDeMatriz
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.Entre = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Entre
+            // 
+            this.Entre.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Entre.BackgroundImage")));
+            this.Entre.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Entre.Location = new System.Drawing.Point(997, 454);
+            this.Entre.Name = "Entre";
+            this.Entre.Size = new System.Drawing.Size(191, 54);
+            this.Entre.TabIndex = 0;
+            this.Entre.Text = "Entre";
+            this.Entre.UseVisualStyleBackColor = true;
+            this.Entre.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1233, 564);
+            this.Controls.Add(this.Entre);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Teal;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Calculadora de Matrizes";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Entre;
+    }
+}
+
